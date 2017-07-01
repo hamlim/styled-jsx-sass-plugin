0f63867a62c29a9d22d317ea391ed4615b6f9079
Matt Hamlin <hamlim@outlook.com>
2017-07-01 10:29:56 -0400

    Supporting imports in a funky way

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 var nodeSass = require('node-sass')
+var fs = require('fs')
 module.exports = function(css, options) {
-	return nodeSass.renderSync({ data: css }).css.toString()
+	var contents = nodeSass.renderSync({
+		data: css,
+		importer: function(url, prev, done) {
+			var file = fs.readFileSync('./srca/' + url)
+			if (!file) {
+				return ''
+			}
+			return { contents: file.toString() }
+		},
+	})
+	return contents.css.toString()
 }
